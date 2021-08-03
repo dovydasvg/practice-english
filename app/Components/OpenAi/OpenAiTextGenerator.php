dde4e0f55b28dbe828f4d740900c708e8475ec15
dovydasvg <dovydasvg@yahoo.fr>
2021-08-03 14:09:09 +0300

    feat: connected to OpenAi API and the generator can fetch some content now

--- app/Components/OpenAi/OpenAiTextGenerator.php
+++ app/Components/OpenAi/OpenAiTextGenerator.php
@@ -9,8 +9,15 @@ use App\Domain\Content\Contracts\ContentProviderInterface;
 class OpenAiTextGenerator implements ContentProviderInterface
 {
 
+    private OpenAiServices $services;
+
+    public function __construct(OpenAiServices $services)
+    {
+        $this->services = $services;
+    }
+
     public function getContent(): string
     {
-        // TODO: Implement getContent() method.
+        return $this->services->fetchRandomSentence();
     }
 }
--- tests/Unit/RawContent/OpenAiTest.php
+++ tests/Unit/RawContent/OpenAiTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit\RawContent;
 
+use App\Components\OpenAi\OpenAiServices;
 use App\Components\OpenAi\OpenAiTextGenerator;
 use App\Domain\Content\Contracts\ContentProviderInterface;
 use PHPUnit\Framework\TestCase;
@@ -10,10 +11,12 @@ class OpenAiTest extends TestCase
 {
 
     private OpenAiTextGenerator $OpenAiGenerator;
+    private OpenAiServices $OpenAiServices;
 
     public function setUp(): void
     {
-        $this->OpenAiGenerator = new OpenAiTextGenerator();
+        $this->OpenAiServices = new OpenAiServices();
+        $this->OpenAiGenerator = new OpenAiTextGenerator($this->OpenAiServices);
     }
 
     public function testThatOpenAiTextGeneratorClassExists(): void
@@ -24,6 +27,11 @@ class OpenAiTest extends TestCase
     public function testThatOpenAiTextGeneratorIsAContentProvider(): void
     {
         $this->assertInstanceOf(ContentProviderInterface::class,$this->OpenAiGenerator);
+    }
+
+    public function testThatOpenAiTextGeneratorCanProvideSomeRandomContent(): void
+    {
+        $this->assertIsString($this->OpenAiGenerator->getContent());
     }
 
 }

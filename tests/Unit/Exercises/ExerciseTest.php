70769aa5066aa3083d41937e72c043c55bd2c2c4
dovydasvg <dovydasvg@yahoo.fr>
2021-09-05 11:33:01 +0300

    small refactor

--- tests/Unit/Exercises/ExerciseTest.php
+++ tests/Unit/Exercises/ExerciseTest.php
@@ -2,10 +2,6 @@
 
 namespace Tests\Unit\Exercises;
 
-use App\Components\OpenAi\OpenAiServices;
-use App\Components\OpenAi\OpenAiTextGenerator;
-use App\Components\RandomQuote\RandomQuoteGenerator;
-use App\Domain\Content\Contracts\ContentProviderInterface;
 use App\Domain\Exercises\Factory\ExerciseFactory;
 use Tests\TestCase;
 
@@ -13,7 +9,7 @@ class ExerciseTest extends TestCase
 {
 
     private const ARTICLE_EXERCISE = 'This is ____ simple sentence with ____ simple task of creating ____ simple exercise in ____ elegant way.';
-    const RAW_SENTENCE = 'This is a simple sentence with the simple task of creating a simple exercise in an elegant way.';
+    private const RAW_SENTENCE = 'This is a simple sentence with the simple task of creating a simple exercise in an elegant way.';
     private ExerciseFactory $ExerciseFactory;
 
     public function setUp(): void
@@ -28,4 +24,5 @@ class ExerciseTest extends TestCase
         $exercise_sentence = $exercise->getWithoutAnswers();
         $this->assertEquals(self::ARTICLE_EXERCISE, $exercise_sentence);
     }
+
 }

5b5bd7e97e911099bd3159b3ef48e99a66a0d3d5
dovydasvg <dovydasvg@yahoo.fr>
2021-09-14 21:46:37 +0300

    feature: adding answer trimming;

--- app/Domain/Exercises/Model/ArticleAbstractExercise.php
+++ app/Domain/Exercises/Model/ArticleAbstractExercise.php
@@ -26,7 +26,23 @@ class ArticleAbstractExercise extends AbstractExercise
     protected function createExercise(string $rawString): Exercise
     {
         $withoutAnswers = preg_replace('/ (a|an|the) /im', ' ____ ', $rawString);
-        preg_match_all('/ a|the /im', $rawString, $output_array);
+        preg_match_all('/ (a|an|the) /im', $rawString, $answers);
+        $output_array = $this->trimAnswers($answers);
         return new Exercise($rawString, $withoutAnswers, $output_array);
+    }
+
+    /**
+     * @param array $answers
+     * @return array
+     */
+    protected function trimAnswers(array $answers): array
+    {
+        if(!empty($answers[0])) {
+            array_walk($answers, function (&$value) {
+
+                $value = trim($value[0]);
+            });
+        }
+        return $answers;
     }
 }

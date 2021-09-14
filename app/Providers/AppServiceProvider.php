48cf85386035ba5d9c56bb66dd4cd956cc77c443
dovydasvg <dovydasvg@yahoo.fr>
2021-09-14 18:43:53 +0300

    refactor: adding depency injection of randomquotegenerator

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Components\RandomQuote\RandomQuoteGenerator;
+use App\Components\RandomQuote\RandomQuoteParser;
+use App\Components\RandomQuote\RandomQuoteService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +16,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->singleton(RandomQuoteGenerator::class, function($app){
+            return new RandomQuoteGenerator(new RandomQuoteService(), new RandomQuoteParser());
+        });
     }
 
     /**
